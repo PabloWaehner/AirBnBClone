fb8e9c4e2bf712b5f5b10b2b36ac903fdde3cbc1
Pablo Waehner <waehnermp@gmail.com>
2021-06-03 11:57:36 +0200

    refactor signup component

--- src/pages/Login/SignUp.js
+++ src/pages/Login/SignUp.js
@@ -7,6 +7,31 @@ import Login from './Login'
 
 class SignUp extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            lowerPartOfForm: <button type="button" onClick={this.showInputs} className="sign-up-button">Sign up with email</button>
+        }
+    }
+
+    changeEmail = (e) => this.setState({ email: e.target.value })
+    changePassword = (e) => this.setState({ password: e.target.value })
+
+    showInputs = () => {
+        this.setState({
+            lowerPartOfForm: <SignUpInputFields
+                changeEmail={this.changeEmail}
+                changePassword={this.changePassword}
+            />
+        })
+    }
+
+    submitLogin = (e) => {
+        e.preventDefault();
+        console.log(this.state.email);
+        console.log(this.state.password);
+    }
+
     render() {
         return (
             <div className="login-form">
@@ -17,7 +42,7 @@ class SignUp extends Component {
                         <span>or</span>
                         <div className="or-divider"></div>
                     </div>
-                    <button className="sign-up-button">Sign up with email</button>
+                    {this.state.lowerPartOfForm}
                     <div className="divider"></div>
                     <div>Already have an account? <span className="pointer" onClick={() => { this.props.openModal('open', <Login />) }}>Log in</span></div>
                 </form>
@@ -35,3 +60,25 @@ function mapDispatchToProps(dispatcher) {
 }
 
 export default connect(null, mapDispatchToProps)(SignUp);
+
+const SignUpInputFields = (props) => {
+    return (
+        <div className="sign-up-wrapper">
+            <div className="col m12">
+                <div className="input-field" id="email">
+                    <div className="form-label">Email</div>
+                    <input type="text" placeholder="Email" onChange={props.changeEmail} />
+                </div>
+            </div>
+            <div className="col m12">
+                <div className="input-field" id="password">
+                    <div className="form-label">Password</div>
+                    <input type="password" placeholder="Password" onChange={props.changePassword} />
+                </div>
+            </div>
+            <div className="col m12">
+                <button type="submit" className="btn red accent-2">Sign Up!</button>
+            </div>
+        </div>
+    )
+}
