4dc6d80a4343afaede19ff1e5025d6a68ab37861
Pablo Waehner <waehnermp@gmail.com>
2021-06-10 12:31:29 +0200

    add styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
 import Home from './pages/Home/Home';
 import NavBar from './utility/NavBar/NavBar';
 import SingleFullVenue from './pages/SingleFullVenue/SingleFullVenue';
